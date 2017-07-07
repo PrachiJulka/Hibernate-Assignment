965f966c88d161dfb4f1c95b0380641bf9d29706
prachijulka <prachijulka31@gmail.com>
2017-07-08 00:20:21 +0530

    Q1

--- workspace/Hibernate1/src/Classes/Author.java
+++ workspace/Hibernate1/src/Classes/Author.java
@@ -21,75 +21,25 @@ import javax.persistence.TemporalType;
 import javax.persistence.Transient;
 
 import org.hibernate.annotations.Cascade;
-
+//Create a class Author with instance variables firstName, lastName and age.
 
 @Entity
-@Table(name="Author")
 public class Author {
 	
 	@Id
 	//@GeneratedValue(strategy = GenerationType.IDENTITY)
-	
-	@Column(name="authID")
 	private int id;
-	
-	@Temporal(TemporalType.DATE)
-	@Column(name="Date")
-	private Date date;
-	
-	@Column(name="FirstName")
 	private String firstName;
-	
-	@Transient
-	@Column(name="LastName")
 	private String lastName;
-	
-	
-	@Embedded
-	private Address add;
-	
-	
-	@OneToMany(mappedBy="auth",cascade=CascadeType.PERSIST)
-	List<Book> buk=new ArrayList<>();
-	
-	public List<Book> getBuk() {
-		return buk;
-	}
-	public void setBuk(List<Book> buk) {
-		this.buk = buk;
-	}
-	@Column(name="Age")
 	private int age;
 
 	
-	@ElementCollection
-	private List<String> ls=new ArrayList<>();
-	public Address getAdd() {
-		return add;
-	}
-	public List<String> getLs() {
-		return ls;
-	}
-	public void setLs(List<String> ls) {
-		this.ls = ls;
-	}
-	public void setAdd(Address add) {
-		this.add = add;
-	}
 	public int getId() {
 		return id;
 	}
 	public void setId(int id) {
 		this.id = id;
 	}
-	
-	public Date getDate() {
-		return date;
-	}
-	public void setDate(Date date) {
-		this.date = date;
-	}
-	
 	
 	public String getFirstName() {
 		return firstName;

0d27ae4cbdf6da83ece88a29fac1d4f4bf86fc10
prachijulka <prachijulka31@gmail.com>
2017-07-08 01:20:16 +0530

    Q6

--- workspace/Hibernate1/src/Classes/Author.java
+++ workspace/Hibernate1/src/Classes/Author.java
@@ -21,16 +21,30 @@ import javax.persistence.TemporalType;
 import javax.persistence.Transient;
 
 import org.hibernate.annotations.Cascade;
-//Create a class Author with instance variables firstName, lastName and age.
+
 
 @Entity
+@Table(name="Author")
 public class Author {
 	
 	@Id
 	//@GeneratedValue(strategy = GenerationType.IDENTITY)
+	
+	@Column(name="authID")
 	private int id;
+	
+	@Temporal(TemporalType.DATE)
+	@Column(name="Date")
+	private Date date;
+	
+	@Column(name="FirstName")
 	private String firstName;
+	
+	@Column(name="LastName")
 	private String lastName;
+	
+	
+	@Column(name="Age")
 	private int age;
 
 	
@@ -40,6 +54,14 @@ public class Author {
 	public void setId(int id) {
 		this.id = id;
 	}
+	
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	
 	
 	public String getFirstName() {
 		return firstName;

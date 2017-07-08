6cc1aeb522866cc7fc800491641d4fb58500ab98
prachijulka <prachijulka31@gmail.com>
2017-07-08 12:45:06 +0530

    Book Class

--- workspace/Hibernate1/src/Classes/Book.java
+++ workspace/Hibernate1/src/Classes/Book.java
@@ -4,16 +4,31 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 
-//Q14 Create an Entity book with an instance variable bookName
+//Q15 Implement One to One mapping between Author and Book.
 @Entity
 public class Book {
-	
+	public int getBid() {
+		return bid;
+	}
+
+	public void setBid(int bid) {
+		this.bid = bid;
+	}
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int bid;
+
 	private String bookName;
 
+	
 
 public String getBookName() {
 	return bookName;

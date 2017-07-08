dbe4df500b68347af2241c98d6f185dfd5293129
prachijulka <prachijulka31@gmail.com>
2017-07-08 13:21:23 +0530

    Q17MainClass

--- workspace/Hibernate1/src/test/Q17.java
+++ workspace/Hibernate1/src/test/Q17.java
@@ -14,38 +14,28 @@ public class Q17 {
 		SessionFactory sess = new Configuration().configure("/resources/Q17.cfg.xml").buildSessionFactory();
 		Author obj=new Author();
 		obj.setId(10);
-		obj.setFirstName("Hello");
-		obj.setLastName("hi");
+		obj.setFirstName("Evan");
+		obj.setLastName("Bayross");
 		obj.setAge(20);
 		Address adr=new Address();
-		adr.setState(9);
+		adr.setState("Delhi");
 		adr.setLocation("Delhi");
 		adr.setStreetNumber(12);
 		obj.setAdd(adr);
 		
-		Author obj1=new Author();
-		obj1.setId(11);
-		obj1.setFirstName("prachi");
-		obj1.setLastName("julka");
-		obj1.setAge(20);
-		Address adr1=new Address();
-		adr1.setState(1);
-		adr1.setLocation("Delhi");
-		adr1.setStreetNumber(11);
-		obj1.setAdd(adr);
+		
 		
 		Book bok=new Book();
 		bok.setBid(01);
 		bok.setBookName("Mathematics");
-		
+		bok.setAuth(obj);
 		Book bok1=new Book();
 		bok1.setBid(02);
 		bok1.setBookName("Science");
 		//bok1.getLs().add(obj);
 		//bok.getLs().add(obj);
+		bok1.setAuth(obj);
 		
-		obj.getBuk().add(bok);
-		obj.getBuk().add(bok1);
 		Session session=sess.openSession();
 		session.beginTransaction();
 			session.save(obj);

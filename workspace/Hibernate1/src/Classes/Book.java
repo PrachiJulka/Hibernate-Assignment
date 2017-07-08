9cf64fc6797f92cea6d945b4d472cab8c17cc27e
prachijulka <prachijulka31@gmail.com>
2017-07-08 14:18:32 +0530

    Q17(BookClass)

--- workspace/Hibernate1/src/Classes/Book.java
+++ workspace/Hibernate1/src/Classes/Book.java
@@ -3,6 +3,7 @@ package Classes;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -19,16 +20,17 @@ public class Book {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int bid;
 	private String bookName;
-	@ManyToOne
-	private Author auth;
-	public Author getAuth() {
-		return auth;
-	}
-	public void setAuth(Author auth) {
-		this.auth = auth;
-	}
+	@ManyToMany
+	private List<Author> auth=new ArrayList<>();
+	
 	public int getBid() {
 		return bid;
+	}
+	public List<Author> getAuth() {
+		return auth;
+	}
+	public void setAuth(List<Author> auth) {
+		this.auth = auth;
 	}
 	public void setBid(int bid) {
 		this.bid = bid;

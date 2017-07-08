118c0f7539a1b252629998b225f278869b8420b9
prachijulka <prachijulka31@gmail.com>
2017-07-08 12:56:02 +0530

    Q16Main Unidirectional

--- workspace/Hibernate1/src/test/Q16.java
+++ workspace/Hibernate1/src/test/Q16.java
@@ -7,37 +7,36 @@ import org.hibernate.cfg.Configuration;
 import Classes.Address;
 import Classes.Author;
 import Classes.Book;
-
+//Q16 Implement One to Many Mapping between Author and Book(Unidirectional, BiDirectional and without additional table ) and  implement cascade save.
+	//Unidirectional
 public class Q16 {
 	public static void main(String[] args){
 		SessionFactory sess = new Configuration().configure("/resources/Q17.cfg.xml").buildSessionFactory();
 		Author obj=new Author();
-		obj.setId(11);
-		obj.setFirstName("Hello");
-		obj.setLastName("hi");
+		obj.setFirstName("Evan");
+		obj.setLastName("Bayross");
 		obj.setAge(20);
 		Address adr=new Address();
-		adr.setState(2);
+		adr.setState("Delhi");
 		adr.setLocation("Delhi");
 		adr.setStreetNumber(11);
 		obj.setAdd(adr);
 		
 		
 		Book bok=new Book();
-		bok.setBid(11);
 		bok.setBookName("Mathematics");
-		bok.setAuth(obj);
 		obj.getBuk().add(bok);
 		
 		Book bok1=new Book();
-		bok1.setBid(02);
 		bok1.setBookName("Science");
-		bok1.setAuth(obj);
 		obj.getBuk().add(bok1);
 		
 		Session session=sess.openSession();
 		session.beginTransaction();
-			session.persist(obj);
+			session.save(obj);
+			session.save(bok);
+
+			session.save(bok1);
 							session.getTransaction().commit();
 				session.close();
 	}

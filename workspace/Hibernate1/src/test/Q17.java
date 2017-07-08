f4091a387a30452bfa092a3091173ecf0fdb8af5
prachijulka <prachijulka31@gmail.com>
2017-07-08 13:59:45 +0530

    Q17(Without table and using cascade)

--- workspace/Hibernate1/src/test/Q17.java
+++ workspace/Hibernate1/src/test/Q17.java
@@ -13,7 +13,6 @@ public class Q17 {
 	public static void main(String[] args){
 		SessionFactory sess = new Configuration().configure("/resources/Q17.cfg.xml").buildSessionFactory();
 		Author obj=new Author();
-		obj.setId(10);
 		obj.setFirstName("Evan");
 		obj.setLastName("Bayross");
 		obj.setAge(20);
@@ -23,24 +22,19 @@ public class Q17 {
 		adr.setStreetNumber(12);
 		obj.setAdd(adr);
 		
-		
-		
 		Book bok=new Book();
-		bok.setBid(01);
 		bok.setBookName("Mathematics");
 		bok.setAuth(obj);
 		Book bok1=new Book();
-		bok1.setBid(02);
 		bok1.setBookName("Science");
-		//bok1.getLs().add(obj);
-		//bok.getLs().add(obj);
 		bok1.setAuth(obj);
+		obj.getBuk().add(bok);
+		obj.getBuk().add(bok1);
+		
 		
 		Session session=sess.openSession();
 		session.beginTransaction();
-			session.save(obj);
-			session.save(bok);
-			session.save(bok1);
+			session.persist(obj);
 				session.getTransaction().commit();
 				session.close();
 	
